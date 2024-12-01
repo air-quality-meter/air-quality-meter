c297764649dcfa1c3e18a1c64778f6ebf2f5e260
Dominik Oetiker <oetikerdominik@gmail.com>
2024-12-01 23:46:26 +0100

    Test Hello

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,9 @@
 #include <Arduino.h>
 // the setup function runs once when you press reset or power the board
 void setup() {
-    // setup function
+    // Hello World
 }
 
 // the loop function runs over and over again forever
 void loop() {
-    // loop function
 }
