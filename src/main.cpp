59f8a66fd9ea6166eb788a66bd6ceee3b2acb10d
Dominik Oetiker <oetikerdominik@gmail.com>
2024-12-02 00:04:39 +0100

    Test für Video

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
 // the setup function runs once when you press reset or power the board
 void setup() {
-    // Hello World
 }
 
 // the loop function runs over and over again forever
