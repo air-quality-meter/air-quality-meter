5eecf7718281909ab79cd2c06f295baa0023283a
Dominik Oetiker <oetikerdominik@gmail.com>
2024-12-05 14:41:10 +0100

    changed variable names and comments to clarify the units

--- src/main.cpp
+++ src/main.cpp
@@ -12,15 +12,15 @@ constexpr int waiting_period_between_warnings_s = 60; // value in seconds
 constexpr int max_consecutive_warnings = 5; // number of maximum consecutive audio warnings
 
 // global variables
-int current_time;
-int current_co2_measurement;
-int last_co2_below_threshold_time;
+int current_time_s;
+int current_co2_measurement_ppm;
+int last_co2_below_threshold_time_s;
 int warning_counter;
 
 // function declarations
-int get_current_time();
+int get_current_time_in_s();
 
-int get_co2_measurement();
+int get_co2_measurement_in_ppm();
 
 void issue_audio_warning();
 
@@ -30,66 +30,66 @@ void set_led(int);
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-    last_co2_below_threshold_time = get_current_time();
+    last_co2_below_threshold_time_s = get_current_time_in_s();
     warning_counter = 0;
     pinMode(BUTTON, INPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-    // set the current_time timestamp
-    current_time = get_current_time();
+    // set the current_time_s timestamp (seconds since board is on)
+    current_time_s = get_current_time_in_s();
 
-    // get the current measurement from co2 sensor
-    current_co2_measurement = get_co2_measurement();
+    // get the current measurement from co2 sensor in ppm
+    current_co2_measurement_ppm = get_co2_measurement_in_ppm();
 
     // display the current co2 measurement on the display
-    display_co2_value(current_co2_measurement);
+    display_co2_value(current_co2_measurement_ppm);
 
     // set the LEDs according to the CO2 measurement value
-    set_led(current_co2_measurement);
+    set_led(current_co2_measurement_ppm);
 
     // check if reset button was pressed
     if (digitalRead(BUTTON) == HIGH) {
         // reset timestamp
-        last_co2_below_threshold_time = current_time;
+        last_co2_below_threshold_time_s = current_time_s;
     }
 
     // check whether the current CO2 measurement is above the threshold value
-    if (current_co2_measurement > co2_threshold_ppm) {
+    if (current_co2_measurement_ppm > co2_threshold_ppm) {
         // check whether the CO2 threshold value has already been exceeded for longer than the maximum period of time
-        if (current_time - last_co2_below_threshold_time > max_co2_above_threshold_time_s) {
+        if (current_time_s - last_co2_below_threshold_time_s > max_co2_above_threshold_time_s) {
             // issue an audio warning
             issue_audio_warning();
 
             // adjust the timestamp to wait until the next audio warning
-            last_co2_below_threshold_time = last_co2_below_threshold_time + waiting_period_between_warnings_s;
+            last_co2_below_threshold_time_s = last_co2_below_threshold_time_s + waiting_period_between_warnings_s;
 
             // increase the warning counter
             warning_counter++;
 
             // reset timestamp when the maximum number of warnings has been reached
             if (warning_counter > max_consecutive_warnings) {
-                last_co2_below_threshold_time = current_time;
+                last_co2_below_threshold_time_s = current_time_s;
             }
         }
     } else {
         // reset timestamp
-        last_co2_below_threshold_time = current_time;
+        last_co2_below_threshold_time_s = current_time_s;
     }
 }
 
 // function definitions
 
-// get the current time since board is on
-int get_current_time() {
+// get the current time since board is on in seconds
+int get_current_time_in_s() {
     return millis();
 };
 
-// get current co2 measurement
-int get_co2_measurement() {
-    int co2_measurement;
-    return co2_measurement;
+// get current co2 measurement in ppm
+int get_co2_measurement_in_ppm() {
+    int co2_measurement_ppm;
+    return co2_measurement_ppm;
 }
 
 // issue an audio warning
