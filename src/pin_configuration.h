b4918b759a1f0c3cb9e8c070828be73e9c53e61d
Dominik Oetiker <oetikerdominik@gmail.com>
2024-12-14 13:00:17 +0100

    pin configuration with constexpr instead of #define. That way it becomes immutable.

--- src/pin_configuration.h
+++ src/pin_configuration.h
@@ -6,13 +6,22 @@
 #ifndef PIN_CONFIGURATION_H
 #define PIN_CONFIGURATION_H
 
-// Pins
-#define TIME_COUNTER_RESET_BUTTON_PIN 2 ///< Interrupt functionality on Pin2 (Int0)
-#define GREEN_LED_1_PIN 22 ///< LED to indicate high air quality on Pin 22
-#define GREEN_LED_2_PIN 24 ///< LED to indicate high or medium air quality on Pin 24
-#define YELLOW_LED_1_PIN 26 ///< LED to indicate medium or moderate air quality on Pin 26
-#define YELLOW_LED_2_PIN 28 ///< LED to indicate moderate air quality on Pin 28
-#define RED_LED_1_PIN 30 ///< LED to indicate moderate or poor air quality on Pin 30
-#define RED_LED_2_PIN 32 ///< LED to indicate poor air quality on Pin 32
+// Reset Button
+constexpr int TIME_COUNTER_RESET_BUTTON_PIN = 2; ///< Interrupt functionality on Pin2 (Int0)
+
+// LEDs
+constexpr int GREEN_LED_1_PIN = 22; ///< LED to indicate high air quality on Pin 22
+constexpr int GREEN_LED_2_PIN = 24; ///< LED to indicate high or medium air quality on Pin 24
+constexpr int YELLOW_LED_1_PIN = 26; ///< LED to indicate medium or moderate air quality on Pin 26
+constexpr int YELLOW_LED_2_PIN = 28; ///< LED to indicate moderate air quality on Pin 28
+constexpr int RED_LED_1_PIN = 30; ///< LED to indicate moderate or poor air quality on Pin 30
+constexpr int RED_LED_2_PIN = 32; ///< LED to indicate poor air quality on Pin 32
+
+// CO2 Sensor
+// TODO CO2 Sensor is on Serial1 and probably don't need PIN declaration?
+
+// Display
+
+// MP3 Module
 
 #endif //PIN_CONFIGURATION_H
