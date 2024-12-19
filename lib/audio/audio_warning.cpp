d34b4c44eaea9d3176f6eddc49cb53526b85dad5
Dominik Oetiker <oetikerdominik@gmail.com>
2024-12-19 12:50:01 +0100

    Use `constexpr` for audio module constants.
    Replaced `unsigned char` with `constexpr uint8_t` for volume and track constants to to fix errors and ensure compile-time evaluation.

--- lib/audio/audio_warning.cpp
+++ lib/audio/audio_warning.cpp
@@ -14,14 +14,14 @@ SoftwareSerial mp3_module(MP3_MODULE_TX_PIN, MP3_MODULE_RX_PIN);
 //void setup() {
 void initialize_mp3_module() {
     mp3_module.begin(9600);
-    const unsigned char vol = 0x1E;
-    unsigned char volume[5] = {0xAA,0x13,0x01,vol,vol+0xBE};
+    constexpr uint8_t vol = 0x1E;
+    constexpr uint8_t volume[5] = {0xAA,0x13,0x01,vol,vol+0xBE};
     mp3_module.write(volume,5);
 }
 
 void issue_audio_warning() {
-  	const unsigned char track = 0x01; ///< Track: "CO2 Wert zu hoch, bitte Fenster öffnen!"
+    constexpr uint8_t track = 0x01; ///< Track: "CO2 Wert zu hoch, bitte Fenster öffnen!"
     //const unsigned char track = 0x03; ///< Track: "Die Luftqualität ist schlecht, bitte Fenster öffnen!"
-    unsigned char play[6] = {0xAA,0x07,0x02,0x00,track,track+0xB3};
+    constexpr uint8_t play[6] = {0xAA,0x07,0x02,0x00,track,track+0xB3};
     mp3_module.write(play,6);
 }
