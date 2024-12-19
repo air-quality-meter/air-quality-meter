7f19c272ee25d06df47eb928e3e118f8c98caa9f
Dominik Oetiker <oetikerdominik@gmail.com>
2024-12-19 16:53:18 +0100

    Add debounce logic for reset button interrupt
    Introduced a debounce mechanism to prevent multiple triggers of the reset button within a short timeframe. Added a timestamp field `last_interrupt_time_ms` to `SystemState` and implemented logic to ignore interrupts occurring within 1000 ms of the last event. This ensures button presses are processed reliably without unintended resets.

--- lib/system_manager/system_manager.cpp
+++ lib/system_manager/system_manager.cpp
@@ -9,12 +9,18 @@
 #include "system_state.h"
 #include "pin_configuration.h"
 
+constexpr unsigned long DEBOUNCE_DELAY_MS = 1000; ///< Time between two interrupts to debounce.
+
 void initialize_reset_button() {
     attachInterrupt(TIME_COUNTER_RESET_BUTTON_PIN, reset_co2_below_threshold_and_warning_counter,FALLING);
 }
 
 void reset_co2_below_threshold_and_warning_counter() {
-    //TODO: debounce reset button.
+    // Debounce: If elapsed time since last interrupt is less than debounce time, ignore this and return
+    const unsigned long interrupt_time_ms = millis(); ///< timestamp in milliseconds
+    if (interrupt_time_ms - system_state.last_interrupt_time_ms < DEBOUNCE_DELAY_MS) {
+        return;
+    }
     system_state.last_co2_below_threshold_time_s = get_current_time_in_s();
     system_state.warning_counter = 0;
 }
