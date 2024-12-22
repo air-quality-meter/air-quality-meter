f92ba93f7d954daeba988246c7513f96519f37ce
Dominik Oetiker <oetikerdominik@gmail.com>
2024-12-22 22:43:42 +0100

    Refactor display code
    Replaced hardcoded values with constants and enum for column, row, and cursor positions. This eliminates magic numbers, and centralizes configuration for easier updates in the future.

--- lib/display/display.cpp
+++ lib/display/display.cpp
@@ -10,6 +10,13 @@
 #include <LiquidCrystal.h> // lib for LCD
 
 constexpr int welcome_message_time_ms = 2000;
+constexpr byte display_n_cols = 16;
+constexpr byte display_n_rows = 2;
+constexpr byte start_col = 0;
+enum Rows : byte {
+    ROW_1 = 0,
+    ROW_2 = 1
+};
 constexpr char welcome_message[] = "Air Quality Meter";
 constexpr char initializing_message[] = "Initializing...";
 
@@ -17,13 +24,13 @@ constexpr char initializing_message[] = "Initializing...";
 LiquidCrystal lcd(LCD_RS_PIN, LCD_EN_PIN, LCD_D4_PIN, LCD_D5_PIN, LCD_D6_PIN, LCD_D7_PIN);
 
 void initialize_display() {
-    lcd.begin(16, 2); // Initialisiere das LCD mit 16 Zeichen und 2 Zeilen
+    lcd.begin(display_n_cols, display_n_rows); // Initialisiere das LCD mit 16 Zeichen und 2 Zeilen
     lcd.clear(); // delete the display content
 
     // display Welcome message
-    lcd.setCursor(0, 0); // first line
+    lcd.setCursor(start_col, Rows::ROW_1); // first line
     lcd.print(welcome_message);
-    lcd.setCursor(0, 1); // second line
+    lcd.setCursor(start_col, Rows::ROW_2); // second line
     lcd.print(initializing_message);
 
     delay(welcome_message_time_ms); // show the message for 2 seconds
@@ -34,10 +41,10 @@ void display_out(const String &line_1, const String &line_2) {
     lcd.clear(); // delete the display content
 
     // display line 1
-    lcd.setCursor(0, 0); // first line
+    lcd.setCursor(start_col, Rows::ROW_1); // first line
     lcd.print(line_1);
 
     // display line 2
-    lcd.setCursor(0, 1); // second line
+    lcd.setCursor(start_col, Rows::ROW_2); // second line
     lcd.print(line_2);
 }
