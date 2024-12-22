af1bde8815ee080618bf1b1e3ddb7633e1562708
Dominik Oetiker <oetikerdominik@gmail.com>
2024-12-22 23:46:39 +0100

    Use consistent naming for constants in display.cpp.
    Updated the naming convention of constants to adhere to an uppercase snake_case style. Adjusted all relevant usages across the file to match the updated names.

--- lib/display/display.cpp
+++ lib/display/display.cpp
@@ -9,31 +9,31 @@
 #include "pin_configuration.h"
 #include <LiquidCrystal.h> // lib for LCD
 
-constexpr int welcome_message_time_ms = 2000;
-constexpr byte display_n_cols = 16;
-constexpr byte display_n_rows = 2;
-constexpr byte start_col = 0;
+constexpr int WELCOME_MESSAGE_TIME_MS = 2000;
+constexpr byte DISPLAY_N_COLS = 16;
+constexpr byte DISPLAY_N_ROWS = 2;
+constexpr byte START_COL = 0;
 enum Rows : byte {
     ROW_1 = 0,
     ROW_2 = 1
 };
-constexpr char welcome_message[] = "Air Quality Meter";
-constexpr char initializing_message[] = "Initializing...";
+constexpr char WELCOME_MESSAGE[] = "Air Quality Meter";
+constexpr char INITIALIZING_MESSAGE[] = "Initializing...";
 
 // Initialisierung des LCD-Displays (RS, EN, D4, D5, D6, D7)
 LiquidCrystal lcd(LCD_RS_PIN, LCD_EN_PIN, LCD_D4_PIN, LCD_D5_PIN, LCD_D6_PIN, LCD_D7_PIN);
 
 void initialize_display() {
-    lcd.begin(display_n_cols, display_n_rows); // Initialisiere das LCD mit 16 Zeichen und 2 Zeilen
+    lcd.begin(DISPLAY_N_COLS, DISPLAY_N_ROWS); // Initialisiere das LCD mit 16 Zeichen und 2 Zeilen
     lcd.clear(); // delete the display content
 
     // display Welcome message
-    lcd.setCursor(start_col, Rows::ROW_1); // first line
-    lcd.print(welcome_message);
-    lcd.setCursor(start_col, Rows::ROW_2); // second line
-    lcd.print(initializing_message);
+    lcd.setCursor(START_COL, ROW_1); // first line
+    lcd.print(WELCOME_MESSAGE);
+    lcd.setCursor(START_COL, ROW_2); // second line
+    lcd.print(INITIALIZING_MESSAGE);
 
-    delay(welcome_message_time_ms); // show the message for 2 seconds
+    delay(WELCOME_MESSAGE_TIME_MS); // show the message for 2 seconds
     lcd.clear(); // delete the display content
 }
 
@@ -41,10 +41,10 @@ void display_out(const String &line_1, const String &line_2) {
     lcd.clear(); // delete the display content
 
     // display line 1
-    lcd.setCursor(start_col, Rows::ROW_1); // first line
+    lcd.setCursor(START_COL, ROW_1); // first line
     lcd.print(line_1);
 
     // display line 2
-    lcd.setCursor(start_col, Rows::ROW_2); // second line
+    lcd.setCursor(START_COL, ROW_2); // second line
     lcd.print(line_2);
 }
