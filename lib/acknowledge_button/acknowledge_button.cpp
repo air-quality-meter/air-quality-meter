590e35efa0b1d9c8227052390172fdd0cfcb2354
Dominik Oetiker <oetikerdominik@gmail.com>
2024-12-29 23:16:15 +0100

    Log additional debug traces for acknowledge button
    Added TRACE logs to monitor key variables during the handling of the acknowledge button logic. This provides better insight into debouncing behavior and timing issues for debugging purposes.

--- lib/acknowledge_button/acknowledge_button.cpp
+++ lib/acknowledge_button/acknowledge_button.cpp
@@ -23,11 +23,15 @@ namespace AcknowledgeButton {
     }
 
     void acknowledge_warning() {
+        Log.infoln(LogController::ACKNOWLEDGE_BUTTON_PRESSED);
         static unsigned long last_interrupt_time_ms = 0;
         ///< Timestamp of last interrupt initialized with static to persist until next function call.
+        TRACE_LN_u(last_interrupt_time_ms);
         const unsigned long interrupt_time_ms = millis(); ///< timestamp in milliseconds
+        TRACE_LN_u(interrupt_time_ms);
         const unsigned long time_delta_between_interrupt_calls = interrupt_time_ms - last_interrupt_time_ms;
         ///< Time elapsed since last interrupt in milliseconds (handels also potential overflow of millis())
+        TRACE_LN_u(time_delta_between_interrupt_calls);
         if (time_delta_between_interrupt_calls < DEBOUNCE_DELAY_MS) {
             // Debounce: If elapsed time since last interrupt is less than debounce time, ignore this and return
             Log.verboseln(LogController::ACKNOWLEDGE_BUTTON_DEBOUNCED);
@@ -35,8 +39,7 @@ namespace AcknowledgeButton {
             return;
         }
         last_interrupt_time_ms = interrupt_time_ms; // updated last interrupt time for debouncing
-
-        Log.infoln(LogController::ACKNOWLEDGE_BUTTON_PRESSED);
+        TRACE_LN_u(last_interrupt_time_ms);
 
         noInterrupts(); // Temporarily disable interrupts while updating system state
         AirQualityMeter::state.last_co2_below_threshold_time_s = TimeController::get_timestamp_s();
