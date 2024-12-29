4eec7fbba70d2c4cfcccf086012e2a052f64e07d
Dominik Oetiker <oetikerdominik@gmail.com>
2024-12-29 16:37:40 +0100

    Refactor logging and improve acknowledge button handling.
    Replaced hardcoded strings with constants from `LogController` for consistency and maintainability. Added a call to log the current system state after handling an acknowledge button press. Enhanced overall readability by aligning declarations and including required headers.

--- lib/acknowledge_button/acknowledge_button.cpp
+++ lib/acknowledge_button/acknowledge_button.cpp
@@ -10,6 +10,8 @@
 #include <../time_controller/time_controller.h>
 #include <pin_configuration.h>
 
+#include "../log_controller/log_controller.h"
+
 namespace AcknowledgeButton {
     constexpr unsigned long DEBOUNCE_DELAY_MS = 1000; ///< Time between two interrupts to debounce.
 
@@ -24,15 +26,21 @@ namespace AcknowledgeButton {
         // Debounce: If elapsed time since last interrupt is less than debounce time, ignore this and return
         const unsigned long interrupt_time_ms = millis(); ///< timestamp in milliseconds
         if (interrupt_time_ms - AirQualityMeter::state.last_interrupt_time_ms < DEBOUNCE_DELAY_MS) {
-            Log.verbose("Acknowledge button debounced.");
+
+            Log.verbose(LogController::ACKNOWLEDGE_BUTTON_DEBOUNCED);
+
             return;
         }
-        Log.info("Acknowledge button pressed.");
+
+        Log.info(LogController::ACKNOWLEDGE_BUTTON_PRESSED);
+
         noInterrupts(); // Temporarily disable interrupts while updating system state
         AirQualityMeter::state.last_co2_below_threshold_time_s = TimeController::get_timestamp_s();
         AirQualityMeter::state.warning_counter = 0;
         AirQualityMeter::state.last_interrupt_time_ms = interrupt_time_ms; // Save the time of this interrupt for debouncing logic
         interrupts(); // Re-enable interrupts
-        Log.info("State updated.");
+
+        Log.verbose(LogController::STATE_UPDATED);
+        LogController::log_current_state();
     }
 }
--- lib/log_controller/log_controller.h
+++ lib/log_controller/log_controller.h
@@ -7,61 +7,78 @@
 namespace LogController {
 #define TRACE_LN_s(variable) Log.traceln("Variable: " #variable " == \"%s\"", variable) ///< Trace variable with name as string (char*).
 
+
 #define TRACE_LN_S(variable) Log.traceln("Variable: " #variable " == \"%S\"", variable) ///< Trace variable with name as string from flash.
+
 
 #define TRACE_LN_c(variable) Log.traceln("Variable: " #variable " == \"%c\"", variable) ///< Trace variable with name as single character.
 
+
 #define TRACE_LN_C(variable) Log.traceln("Variable: " #variable " == \"%C\"", variable) ///< Trace variable with name as single character or as hexadecimal value (prefixed by `0x`) if not a printable character.
+
 
 #define TRACE_LN_d(variable) Log.traceln("Variable: " #variable " == %d", variable) ///< Trace variable with name as integer.
 
+
 #define TRACE_LN_l(variable) Log.traceln("Variable: " #variable " == %l", variable) ///< Trace variable with name as long.
+
 
 #define TRACE_LN_u(variable) Log.traceln("Variable: " #variable " == %u", variable) ///< Trace variable with name as unsigned long.
 
+
 #define TRACE_LN_x(variable) Log.traceln("Variable: " #variable " == %x", variable) ///< Trace variable with name as hexadecimal value.
+
 
 #define TRACE_LN_X(variable) Log.traceln("Variable: " #variable " == %X", variable) ///< Trace variable with name as hexadecimal value prefixed by `0x` and leading zeros.
 
+
 #define TRACE_LN_b(variable) Log.traceln("Variable: " #variable " == %b", variable) ///< Trace variable with name as binary value.
+
 
 #define TRACE_LN_B(variable) Log.traceln("Variable: " #variable " == %B", variable) ///< Trace variable with name as binary value prefixed by `0b`.
 
+
 #define TRACE_LN_t(variable) Log.traceln("Variable: " #variable " == \'%t\'", variable) ///< Trace variable with name as boolean value "t" or "f".
+
 
 #define TRACE_LN_T(variable) Log.traceln("Variable: " #variable " == \'%T\'", variable) ///< Trace variable with name as boolean value "true" or "false".
 
+
 #define TRACE_LN_F(variable) Log.traceln("Variable: " #variable " == %F", variable) ///< Trace variable with name as floating point.
+
 
 #define TRACE_LN_D(variable) Log.traceln("Variable: " #variable " == %D", variable) ///< Trace variable with name as double.
 
-    constexpr char WELCOME_MESSAGE[]        = "*           Welcome to the Air Quality Meter!           *";
-    constexpr char INIT[]                   = "initialized";
 
-    constexpr char LOG_CONTROLLER[]         = "Log controller";
-    constexpr char DISPLAY_CONTROLLER[]     = "Display controller";
-    constexpr char ACKNOWLEDGE_BUTTON[]     = "Acknowledge button";
-    constexpr char SENSOR_CONTROLLER[]      = "Sensor controller";
-    constexpr char LED_ARRAY[]              = "LED array";
-    constexpr char AUDIO_CONTROLLER[]       = "Audio controller";
+    constexpr char WELCOME_MESSAGE[] = "*           Welcome to the Air Quality Meter!           *";
+    constexpr char INIT[] = "initialized";
 
-    constexpr char SYSTEM_READY[]           = "System ready";
+    constexpr char LOG_CONTROLLER[] = "Log controller";
+    constexpr char DISPLAY_CONTROLLER[] = "Display controller";
+    constexpr char ACKNOWLEDGE_BUTTON[] = "Acknowledge button";
+    constexpr char SENSOR_CONTROLLER[] = "Sensor controller";
+    constexpr char LED_ARRAY[] = "LED array";
+    constexpr char AUDIO_CONTROLLER[] = "Audio controller";
 
-    constexpr char STATE[]                  = "Current State:";
+    constexpr char SYSTEM_READY[] = "System ready";
 
-    constexpr char LOOP_START[]             = "Loop start";
-    constexpr char LOOP_END[]               = "Loop end";
+    constexpr char STATE[] = "Current State:";
 
-    constexpr char SENSOR_ERROR[]           = "Sensor error";
+    constexpr char LOOP_START[] = "Loop start";
+    constexpr char LOOP_END[] = "Loop end";
 
-    constexpr char LED_UPDATED[]            = "LED array updated";
-    constexpr char DISPLAY_UPDATED[]        = "Display updated";
-    constexpr char AUDIO_WARNING_ISSUED[]   = "Audio warning issued";
-    constexpr char STATE_UPDATED[]          = "State updated";
+    constexpr char SENSOR_ERROR[] = "Sensor error";
 
-    constexpr char DIVIDING_LINE_WELCOME[]  = "*********************************************************";
-    constexpr char DIVIDING_LINE_LOOP[]     = "#########################################################";
-    constexpr char DIVIDING_LINE_STATE[]    = "---------------------------------------------------------";
+    constexpr char LED_UPDATED[] = "LED array updated";
+    constexpr char DISPLAY_UPDATED[] = "Display updated";
+    constexpr char AUDIO_WARNING_ISSUED[] = "Audio warning issued";
+    constexpr char STATE_UPDATED[] = "State updated";
+    constexpr char ACKNOWLEDGE_BUTTON_DEBOUNCED[] = "Acknowledge button debounced";
+    constexpr char ACKNOWLEDGE_BUTTON_PRESSED[] = "Acknowledge button pressed";
+
+    constexpr char DIVIDING_LINE_WELCOME[] = "*********************************************************";
+    constexpr char DIVIDING_LINE_LOOP[] = "#########################################################";
+    constexpr char DIVIDING_LINE_STATE[] = "---------------------------------------------------------";
 
     void initialize(int log_level);
 
