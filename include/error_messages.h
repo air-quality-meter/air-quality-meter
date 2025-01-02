e8ec431827a0dacea0a4e9e48aa18ea0b135f912
Dominik Oetiker <oetikerdominik@gmail.com>
2025-01-02 11:20:15 +0100

    Add detailed documentation to error_messages.h

--- include/error_messages.h
+++ include/error_messages.h
@@ -1,13 +1,26 @@
+/**
+   * @file error_messages.h
+   * @brief Centralized error message definitions for general and sensor-specific issues.
+   *
+   * @details This file organizes reusable error messages related to sensor conditions or
+   * generic failures.
+   */
+
 #ifndef ERROR_MESSAGES_H
 #define ERROR_MESSAGES_H
 
 namespace GeneralError {
+    // Generic error messages.
     constexpr char ERROR_MESSAGE_ROW_ONE[] = "Error";
+    ///< Generic error message.
 }
 
 namespace SensorError {
+    // Error messages for the sensor.
     constexpr char NOT_CONNECTED[] = "Sensor not connected";
+    ///< Error message, which indicates, that the sensor is not connected.
     constexpr char MEASUREMENT_NOT_VALID[] = "Measurement not valid";
+    ///< Error message, which indicates, that the measurement is not valid.
 }
 
 #endif //ERROR_MESSAGES_H
