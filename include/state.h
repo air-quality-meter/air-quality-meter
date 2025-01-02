218ec852f92036b6b77b56faa77b0010e1461628
Dominik Oetiker <oetikerdominik@gmail.com>
2025-01-02 11:35:28 +0100

    Add descriptive comments for state.h

--- include/state.h
+++ include/state.h
@@ -1,3 +1,11 @@
+/**
+   * @file state.h
+   * @brief Defines the state structure and global state instance for the air quality meter.
+   *
+   * @details This file contains the `State` structure used to track time-dependent and
+   * warning-related system metrics, along with a global state instance to be shared
+   * across the application.
+   */
 
 #ifndef STATE_H
 #define STATE_H
@@ -5,12 +13,12 @@
 
 namespace AirQualityMeter {
   struct State {
-    volatile unsigned long last_co2_below_threshold_time_s;
-    volatile int warning_counter;
-    volatile unsigned long last_co2_sensor_used_time_stamp_ms;
+    volatile unsigned long last_co2_below_threshold_time_s; ///< Last time (in sec) when CO2 was below threshold.
+    volatile int warning_counter; ///< Counter tracking the number of warnings triggered.
+    volatile unsigned long last_co2_sensor_used_time_stamp_ms; ///< Last time (in ms) the CO2 sensor was activated.
   };
 
-  extern State state;
+  extern State state; ///< Global state instance to manage runtime metrics and warnings.
 }
 
 #endif //STATE_H
