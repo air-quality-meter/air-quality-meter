267b6be70164d366495a7e26598e3739fef7f073
Dominik Oetiker <oetikerdominik@gmail.com>
2025-01-09 09:20:41 +0100

    Increase debounce delay to 1000ms.
    Updated the debounce delay to 1000ms to reduce sensitivity to rapid button presses.

--- lib/button_debouncer/button_debouncer.cpp
+++ lib/button_debouncer/button_debouncer.cpp
@@ -7,7 +7,7 @@
 #include <Arduino.h>
 
 namespace ButtonDebouncer {
-    constexpr unsigned long DEBOUNCE_DELAY_MS = 50; ///< Time between two interrupts to debounce.
+    constexpr unsigned long DEBOUNCE_DELAY_MS = 1000; ///< Time between two button presses to debounce.
     bool is_button_debounced(unsigned long &last_button_press_detected_ms) {
         const unsigned long current_time = millis();
         const unsigned long time_delta_between_detected_button_press_ms = current_time - last_button_press_detected_ms;
